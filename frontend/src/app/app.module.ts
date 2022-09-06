f2d9d2cf03f6b5d6240bd5c49711e69007243864
Albert_Remote <albert_depaula@yahoo.com.br>
2022-09-05 22:34:02 -0300

    Criado o component nav-bar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,25 +1,33 @@
-import {MatTableModule} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule} from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EngineListComponent } from './components/engines/engine-list/engine-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './components/template/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EngineListComponent,
+    NavBarComponent,
   ],
   imports: [
+    MatTableModule,
+    MatToolbarModule,
+    MatSidenavModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatTableModule,
-    HttpClientModule
+    HttpClientModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
