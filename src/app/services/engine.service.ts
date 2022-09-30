d07cff98f1cc6845bdcc43447f51b4973a027bb5
Albert de Paula <albert_depaula@yahoo.com.br>
2022-09-30 11:28:31 -0300

    Alterada a estrutura de pastas para deploy em produção

--- src/app/services/engine.service.ts
+++ src/app/services/engine.service.ts
@@ -9,7 +9,8 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 
 export class EngineService {
-  private apiUrl = 'http://localhost:3333/engines';
+  private apiUrl = 'https://order-helper.herokuapp.com/engines/';
+  //private apiUrl = 'http://localhost:3333/engines';
 
   constructor(private http: HttpClient, private MatSnackBar: MatSnackBar) { }
 
